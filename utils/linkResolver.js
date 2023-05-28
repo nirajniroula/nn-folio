b23c5a7eb54a56840c12e9ad7e0aa6fc9b9d56c4
Niraj Niroula <niniroula@gmail.com>
2023-05-28 21:24:14 +0545

    optimize seo - add sitemap.xml and robots.txt file

---
+++ utils/linkResolver.js
@@ -0,0 +1,15 @@
+// Manages the url links to internal Prismic documents
+// Expand this function as your website grows
+const linkResolver = (doc) => {
+  if (doc.type === "page" && doc.uid !== "home") {
+    return `/${doc.uid}/`;
+  }
+
+  if (doc.type === "page") {
+    return `/${doc.uid}`;
+  }
+
+  return "/";
+};
+
+export default linkResolver;
