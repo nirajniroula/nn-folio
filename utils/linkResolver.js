00b49ffe0203112b6b25cbfa6908a304358d6c2f
Niraj Niroula <niniroula@gmail.com>
2023-05-29 20:27:29 +0545

    update sitemap

--- utils/linkResolver.js
+++ utils/linkResolver.js
@@ -2,7 +2,7 @@
 // Expand this function as your website grows
 const linkResolver = (doc) => {
   if (doc.type === "page" && doc.uid !== "home") {
-    return `/${doc.uid}/`;
+    return `/${doc.uid}`;
   }
 
   if (doc.type === "page") {
