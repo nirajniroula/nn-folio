30005ccb59fe2a1b3ffe0edf48c861fb990244e1
Niraj Niroula <niniroula@gmail.com>
2024-02-02 14:48:55 +0545

    improvise project details ui

--- slices/TextWithImage/index.js
+++ slices/TextWithImage/index.js
@@ -11,12 +11,23 @@ const TextWithImage = ({ slice }) => {
       <div className="grid grid-cols-1 items-start gap-16 md:grid-cols-2">
         <div>
           {prismicH.isFilled.image(image) && (
-            <div className="max-h-80 card overflow-hidden border-base-100 bg-gray-100">
+            <div className="group card relative max-h-80 overflow-hidden border-base-100 bg-gray-100">
               <PrismicNextImage
                 field={image}
                 sizes="100vw"
                 className="w-full"
               />
+              <div className="overlay absolute inset-0 bg-black opacity-0 transition-opacity group-hover:opacity-50"></div>
+              <div className="button-component absolute inset-0 flex items-center justify-center opacity-0 transition-opacity group-hover:opacity-100">
+                {prismicH.isFilled.link(slice.primary.button_link) && (
+                  <PrismicLink
+                    field={slice.primary.button_link}
+                    className="rounded bg-white px-5 py-3 text-center font-medium text-slate-800"
+                  >
+                    {slice.primary.button_text || "View Project"}
+                  </PrismicLink>
+                )}
+              </div>
             </div>
           )}
         </div>
@@ -28,19 +39,9 @@ const TextWithImage = ({ slice }) => {
                 {slice.primary.platform}
               </div>
             </h2>
-            <p className="line-clamp-3">
+            <p className="line-clamp-5">
               <PrismicRichText field={slice.primary.about} />
             </p>
-            <div className="my-8 flex">
-              {prismicH.isFilled.link(slice.primary.button_link) && (
-                <PrismicLink
-                  field={slice.primary.button_link}
-                  className="w-full rounded bg-white px-5 py-3 text-center font-medium text-slate-800 md:w-auto"
-                >
-                  {slice.primary.button_text || "View Project"}
-                </PrismicLink>
-              )}
-            </div>
           </div>
 
           <div className="card-actions mt-4 justify-end">
@@ -52,8 +53,8 @@ const TextWithImage = ({ slice }) => {
           </div>
         </div>
       </div>
-      <h2 className="card-title mb-4 mt-10">Responsibilities/Contribution</h2>
-      <div className="mb-8  h-[0.5px] w-full bg-gray-400"></div>
+      <h2 className="card-title mb-4 mt-16">Responsibilities</h2>
+      <div className="mb-8  h-[1px] w-full bg-gray-400"></div>
       <p className="">
         <PrismicRichText field={slice.primary.text} />
       </p>
